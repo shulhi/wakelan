0a134dce47de2f8b70a9f000017751c473beaa47
Shulhi Sapli <shulhi@gmail.com>
2019-05-21 14:10:06 +0800

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::num::ParseIntError;
+use std::str::FromStr;
 
 /*
  *
@@ -6,23 +8,10 @@ use std::env;
  */
 
 fn main() {
-    let mac_addr: String = env::args().nth(1).unwrap();
-    let mac_addr: &str = &mac_addr;
+    let mac_addr: String = env::args().nth(1).expect("wakelan <MAC>");
+    let mac_addr: MacAddr = (&mac_addr).parse().expect("Invalid MAC address");
 
     println!("{:x?}", mac_addr);
-
-    let parsed_mac_addr = mac_addr.split(|c| c == ':' || c == '-').enumerate().fold(
-        [0; 6],
-        |mut acc, (idx, byte)| match u8::from_str_radix(byte, 16) {
-            Err(_err) => acc,
-            Ok(v) => {
-                acc[idx] = v;
-                acc
-            }
-        },
-    );
-
-    println!("{:x?}", parsed_mac_addr);
 }
 
 /*
@@ -35,4 +24,28 @@ fn main() {
  */
 pub struct WOLPacket {
     packet: [u8; 102],
+}
+
+#[derive(Debug)]
+pub struct MacAddr {
+    addr: [u8; 6],
+}
+
+impl FromStr for MacAddr {
+    type Err = ParseIntError;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        let parsed = value.split(|c| c == ':' || c == '-').enumerate().fold(
+            [0; 6],
+            |mut acc, (idx, byte)| match u8::from_str_radix(byte, 16) {
+                Err(_err) => acc,
+                Ok(v) => {
+                    acc[idx] = v;
+                    acc
+                }
+            },
+        );
+
+        Ok(MacAddr { addr: parsed })
+    }
 }
